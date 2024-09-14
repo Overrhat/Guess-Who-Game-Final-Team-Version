9bd2241f6f93028ef7e123e0b0290c3368a0cb87
Harry <hma481@aucklanduni.ac.nz>
2024-09-14 19:46:48 +1200

    gave the start rectangle in the fxml file a fxid, and now the game starts in the menu and switches to the main room once the button is pressed.

---
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -0,0 +1,43 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameStateContext;
+import nz.ac.auckland.se206.speech.TextToSpeech;
+
+public class MenuController {
+  @FXML private Rectangle rectStart;
+
+  private static boolean isFirstTimeInit = true;
+  private static GameStateContext context = new GameStateContext();
+
+  /**
+   * Initializes the menu. If it's the first time initialization, it will provide instructions
+   * via text-to-speech.
+   */
+  @FXML
+  public void initialize() {
+    if (isFirstTimeInit) {
+      System.out.println("test"); // we can make it say tts with this code when they first load the game
+      isFirstTimeInit = false;
+    }
+    
+  }
+
+  /**
+   * Handles mouse clicks on rectangles representing start button.
+   *
+   * @param event the mouse event triggered by clicking a rectangle
+   */
+  @FXML
+  private void handleStartClick(MouseEvent event) {
+    try {
+      App.setRoot("mainRoom");
+    } catch (Exception e) {
+      System.out.println("Error loading mainRoom.fxml");
+      System.exit(0);
+    }
+  }
+}
