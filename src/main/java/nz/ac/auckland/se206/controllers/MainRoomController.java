12a7590d204e35b52d2ade1118a30f2adfc3517f
Harry <hma481@aucklanduni.ac.nz>
2024-09-16 20:17:30 +1200

    added fxid for all the needed fxml stuff and created the MainRoomController class

--- src/main/java/nz/ac/auckland/se206/controllers/MainRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainRoomController.java
@@ -1,5 +1,24 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+
 public class MainRoomController {
-  
+  @FXML private Label lblTime;
+  @FXML private TextArea txtaChat;
+  @FXML private TextField txtaInput;
+  @FXML private Button btnGuess;
+  @FXML private Button btnSend;
+  @FXML private Rectangle rectOldMan;
+  @FXML private Rectangle rectYoungMan;
+  @FXML private Rectangle rectWoman;
+  @FXML private Rectangle rectCrimeScene;
+  @FXML private Rectangle rectPiano;
+  @FXML private Rectangle rectCase;
+  @FXML private Rectangle rectFootprint;
+
 }
