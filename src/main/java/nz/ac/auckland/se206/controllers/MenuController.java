e68d360f5199c358e490059b6e2292026b8e2297
Jeruh1105 <naruh061105@gmail.com>
2024-10-08 23:10:56 +1300

    The ordering problem has now been fixed

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -22,6 +22,37 @@ public class MenuController {
   public static MediaPlayer mediaPlayer;
   private static boolean isFirstTimeInit = true;
 
+  // Static methods
+  /**
+   * Plays media from the specified file path. If a media file is already playing, it stops the
+   * current playback before starting the new one.
+   *
+   * @param filePath The path to the media file to be played. This should be a valid resource path.
+   * @throws IllegalArgumentException if the file path is null or the media file cannot be found.
+   * @throws MediaException if an error occurs while trying to play the media.
+   *     <p>Note: This method uses JavaFX's {@link Media} and {@link MediaPlayer} classes to play
+   *     media files. It also handles potential exceptions such as {@link URISyntaxException} and
+   *     {@link NullPointerException}.
+   */
+  public static void playMedia(String filePath) {
+    try {
+      // If sound is playing, turn off the sound
+      if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+        mediaPlayer.stop();
+      }
+
+      Media sound = new Media(App.class.getResource(filePath).toURI().toString());
+      mediaPlayer = new MediaPlayer(sound);
+      mediaPlayer.play();
+    } catch (URISyntaxException e) {
+      System.err.println("Invalid URI syntax: " + e.getMessage());
+    } catch (NullPointerException e) {
+      System.err.println("Resource not found: " + filePath);
+    } catch (Exception e) {
+      System.err.println("An error occurred while trying to play media: " + e.getMessage());
+    }
+  }
+
   // Instance fields
   @FXML private Line bottomLine;
   @FXML private QuadCurve leftLine;
@@ -29,6 +60,7 @@ public class MenuController {
   @FXML private QuadCurve rightLine;
   @FXML private Line topLine;
 
+  // Instance methods
   /**
    * Initializes the menu. If it's the first time initialization, it will provide instructions via
    * text-to-speech.
@@ -82,37 +114,6 @@ public class MenuController {
       // Print error loading room.
       System.out.println("Error loading mainRoom.fxml");
       System.exit(0);
-    }
-  }
-
-  /**
-   * Plays media from the specified file path. If a media file is already playing, it stops the
-   * current playback before starting the new one.
-   *
-   * @param filePath The path to the media file to be played. This should be a valid resource path.
-   * @throws IllegalArgumentException if the file path is null or the media file cannot be found.
-   * @throws MediaException if an error occurs while trying to play the media.
-   *
-   *     <p>Note: This method uses JavaFX's {@link Media} and {@link MediaPlayer} classes to play
-   *     media files. It also handles potential exceptions such as {@link URISyntaxException} and
-   *     {@link NullPointerException}.
-   */
-  public static void playMedia(String filePath) {
-    try {
-      // If sound is playing, turn off the sound
-      if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
-        mediaPlayer.stop();
-      }
-
-      Media sound = new Media(App.class.getResource(filePath).toURI().toString());
-      mediaPlayer = new MediaPlayer(sound);
-      mediaPlayer.play();
-    } catch (URISyntaxException e) {
-      System.err.println("Invalid URI syntax: " + e.getMessage());
-    } catch (NullPointerException e) {
-      System.err.println("Resource not found: " + filePath);
-    } catch (Exception e) {
-      System.err.println("An error occurred while trying to play media: " + e.getMessage());
     }
   }
 
